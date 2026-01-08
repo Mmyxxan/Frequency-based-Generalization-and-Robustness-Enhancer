e20c7813d5b9964057b43e780e0ce18f4a9bba26
Mmyxxan <127069162+Mmyxxan@users.noreply.github.com>
2026-01-08 18:26:50 +0700

    Update trainer.py

--- trainers/trainer.py
+++ trainers/trainer.py
@@ -1000,8 +1000,8 @@ class RoHLTrainer(AbstractTrainer):
         high_freq_transform = []
         for i, tf in enumerate(original_transform):
             if i == 1:
-                # high_freq_transform += [transforms.GaussianBlur(kernel_size=3, sigma=(0.1, 3.0))] # AM_{TV}-ft_{Gauss}
-                high_freq_transform += [v2.JPEG(quality=(30, 100))]
+                high_freq_transform += [transforms.RandomApply([transforms.GaussianBlur(kernel_size=3, sigma=(0.1, 3.0))], p=0.5)] # AM_{TV}-ft_{Gauss}
+                high_freq_transform += [transforms.RandomApply([v2.JPEG(quality=(30, 100))], p=0.5)]
             high_freq_transform.append(tf)
             if i == 1:
                 # high_freq_transform += [v2.GaussianNoise(mean=0, sigma=0.08)]
